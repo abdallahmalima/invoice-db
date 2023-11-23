3d93860e9c19f81002d34061bad8ac566c87f4c7
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-11-23 10:02:55 -0500

    dsffds

--- app/api/sms/route.ts
+++ app/api/sms/route.ts
@@ -48,6 +48,7 @@ export async function POST(request: Request) {
         console.log(data, api_key + ":" + secret_key);
       } catch (error) {
         console.error(error);
+        return new Response('Error')
       }
     };
     
