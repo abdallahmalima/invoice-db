5991db38474d645589f2b1892e130564927c70ef
Abdallah Antony <abdallahantony55.aa@gmail.com>
2023-11-24 03:16:10 -0500

    sdggf

--- actions/server.ts
+++ actions/server.ts
@@ -30,7 +30,7 @@ export const sendClientSms= async (formData:any)=>{
             source_addr: source_addr,
             schedule_time: "",
             encoding: 0,
-            message: "Ndugu "+clientName+", SamakiSamaki tunashukuru kwa maoni yako!",
+            message: "Ndugu Mteja, SamakiSamaki tunashukuru kwa maoni yako!",
             recipients: [
               {
                 recipient_id: 1,
